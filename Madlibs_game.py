66ee3544125b700419b97c9260acfd45b016caba
Ronak_Kadyan <ronakkadyan28@Gmail.com>
2025-04-28 01:03:14 +0530

    Add files via upload

---
+++ Madlibs_game.py
@@ -0,0 +1,13 @@
+#madlibs is a game where you creat a story by filling the blank with random words
+
+adjective1 = input("enter an adjective (discription):")
+adjective2 = input("enter an adjective (discription):")
+adjective3 = input("enter an adjective (discription):")
+noun1 = input("enter a noun (name,place,thing)")
+noun2 = input("enter a noun (name,place,thing)")
+verb1 = input("enter a verb (helping word)")
+
+print(f"I went to a {adjective1} zoo")
+print(f"In an exhibition, I saw {noun1}")
+print(f"{noun2} was {adjective2} and {verb1}")
+print(f"I was {adjective3}")
