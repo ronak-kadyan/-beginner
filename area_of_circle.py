39c1268e50b7dfb1cb96c7d6c0384174d2b3a40c
Ronak_Kadyan <ronakkadyan28@Gmail.com>
2025-04-29 00:10:42 +0530

    Add files via upload

---
+++ area_of_circle.py
@@ -0,0 +1,4 @@
+radius = float(input("enter the radius of the circle:"))
+area = math.pi * pow(radius , 2)
+
+print(f"the area of the circle is : {round(area , 3)}")
---
+++ calculator_for_circumferenece.py
@@ -0,0 +1,6 @@
+import math
+
+radius = float(input("enter the radius of the circle:"))
+circumferenece = 2 * math.pi * radius
+
+print(f"the circumference is {circumferenece}")
